00cde9c34eb2e5e312983ef6c9e985486a2711a7
minsub4400 <73373884+minsub4400@users.noreply.github.com>
2022-07-08 17:02:41 +0900

    Player_Map
    Player_Map add

---
+++ Assets/Script/Player/PlayerHealth.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public void Die()
+    {
+        // 게임 오브젝트 비활성화
+        gameObject.SetActive(false);
+        // gameObject는 자기 자신의 게임 오브젝트다.
+    }
+}
---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // PlayerInput를 가져와야한다
+    private PlayerInput myInput;
+    // 이동을 위한 리지드바디를 가져온다
+    private Rigidbody myRigid;
+    // 이동 속도
+    public float speed;
+    void Start()
+    {
+        myInput = GetComponent<PlayerInput>();
+        myRigid = GetComponent<Rigidbody>();
+        // 자신이 포함되어있는 컴퍼넌트 중에 PlayerInput를 찾아서 input에 넣는다.
+        speed = 8.0f;
+    }
+
+    void Update()
+    {
+        if (myInput.Up == true)
+        {
+            myRigid.AddForce(0f, 0f, speed);
+        }
+        if (myInput.Down == true)
+        {
+            myRigid.AddForce(0f, 0f, -speed);
+        }
+        if (myInput.Left == true)
+        {
+            myRigid.AddForce(-speed, 0f, 0f);
+        }
+        if (myInput.Right == true)
+        {
+            myRigid.AddForce(speed, 0f, 0f);
+        }
+    }
+
+} // 클래스 끝.
